5a686c7e39279664ea2ca02828b3d5dc7827c1fd
Atulmahajan12345 <atulmahajan2299@gmail.com>
2022-08-26 21:55:31 +0530

    Added:Do the Arithmetic Operation c+a/b

---
+++ src/com/bridgelabz/ArithmeticOperationSecond.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+public class ArithmeticOperationSecond {
+	public static void main(String[] args) {
+		//ii)c+a/b
+				int a=2,b=3,c=4;
+				int sum=c+a/b;
+				int divide=sum;
+				System.out.println(divide);
+				}
+			
+
+}
