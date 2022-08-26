3f4e3576339ac88eb241b79d0bea62f63920b34a
Atulmahajan12345 <atulmahajan2299@gmail.com>
2022-08-26 17:36:42 +0530

    Added: Program for the sum of n natural numbers using while loop

---
+++ src/com/bridgelabz/SumOfNaturalNumberWhileLoop.java
@@ -0,0 +1,16 @@
+package com.bridgelabz;
+
+public class SumOfNaturalNumberWhileLoop {
+	public static void main(String[] args) {
+		int num = 5, i = 1, sum = 0;
+
+		while (i <= num) {
+
+			sum = sum + i;
+
+			i++;
+		}
+
+		System.out.println("Sum of First 5 Natural Numbers is = " + sum);
+	}
+}
