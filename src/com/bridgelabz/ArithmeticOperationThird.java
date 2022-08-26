258964c76e37c9a231fc3ac3d2e11b733770839a
Atulmahajan12345 <atulmahajan2299@gmail.com>
2022-08-26 22:02:19 +0530

    Added:Do Arithmetic Operation a%b+c

---
+++ src/com/bridgelabz/ArithmeticOperationThird.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+public class ArithmeticOperationThird {
+	public static void main(String[] args) {
+		// a%b+c
+		int a = 2, b = 3, c = 4;
+		int sum = a % b + c;
+		int per = sum;
+		System.out.println(per);
+	}
+
+}
