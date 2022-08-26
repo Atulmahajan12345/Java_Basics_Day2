1f1784a3995871b32ddd3ee7127e8dcef0ae0c18
Atulmahajan12345 <atulmahajan2299@gmail.com>
2022-08-26 17:26:46 +0530

    Added: Program for the sum of n natural numbers

---
+++ src/com/bridgelabz/SumOfNaturalNumberForLoop.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+public class SumOfNaturalNumberForLoop {
+	public static void main(String[] args) {
+		int i, num = 5, sum = 0;
+
+		for (i = 1; i <= num; ++i) {
+
+			sum = sum + i;
+		}
+
+		System.out.println("Sum of First 5 Natural Numbers is = " + sum);
+	}
+}
